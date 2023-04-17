f3d648c9f2da83ef388cd227aec35b3f86ded04d
baonguyen2310 <1phe.com@gmail.com>
2023-04-17 15:32:40 +0700

    quote-generator

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,13 +1,15 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter-btn");
+const newQuoteBtn = document.getElementById("new-quote-btn");
+const translateBtn = document.getElementById("translate-btn");
+const aloudBtn = document.getElementById("aloud-btn");
 const loader = document.getElementById("loader");
 
-let apiQuotes = [];
+let quoteList = [];
+let quote = '';
 
-// Loading
 function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
@@ -18,46 +20,88 @@ function removeLoadingSpinner() {
     quoteContainer.hidden = false;
 }
 
-// Show New Quote
-function newQuote() {
+async function newQuote() {
     const LONG_QUOTE_LENGTH = 120;
-    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    quote = quoteList[Math.floor(Math.random()*quoteList.length)];
+
+    /* Generate quote.textVi */
+    quote.currentLanguage = 'en';
+    const urlTranslate = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+ "en" + "&tl=" + "vi" + "&dt=t&q=" + encodeURI(quote.text);
+    const resTranslate = await fetch(urlTranslate);
+    const targetText = await resTranslate.json();
+    quote.textVi = '';
+    targetText[0].forEach((value, index) => {
+        quote.textVi += value[0]; //sentence
+    })
+
     if (!quote.author) {
         authorText.textContent = "Unknown";
     } else {
         authorText.textContent = quote.author;
     }
     if (quote.text.length > LONG_QUOTE_LENGTH) {
-        quoteText.classList.add('long-quote');
+        quoteText.classList.add('long-text');
     } else {
-        quoteText.classList.remove('long-quote');
+        quoteText.classList.remove('long-text');
     }
     quoteText.textContent = quote.text;
+    aloudQuote();
 }
 
-// Get Quotes From API
 async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
+    const QUOTE_URL = "https://type.fit/api/quotes";
     try {
         showLoadingSpinner();
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
+        const response = await fetch(QUOTE_URL);
+        quoteList = await response.json();
         newQuote();
         removeLoadingSpinner();
     } catch (error) {
-        
-    } finally {
+
     }
 }
 
-// Tweet quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl, '_blank');
+    const tweet = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(tweet, '_blank');
+}
+
+async function translateQuote() {
+    if (quote.currentLanguage === 'en') {
+        quoteText.textContent = quote.textVi;
+        quote.currentLanguage = 'vi';
+    } else {
+        quoteText.textContent = quote.text;
+        quote.currentLanguage = 'en';
+    }
+    aloudQuote();
+}
+
+function aloudQuote() {
+    speechSynthesis.cancel();
+    const speechSynthesisObj = new SpeechSynthesisUtterance(quoteText.textContent + '...' + authorText.textContent);
+    const voices = window.speechSynthesis.getVoices();
+    if (quote.currentLanguage === 'en') {
+        speechSynthesisObj.voice = voices.find((voice, index) => {
+            return voice.lang === 'en-US';
+        });
+        speechSynthesisObj.rate = 0.75;
+        speechSynthesisObj.pitch = 0.75;
+    } else {
+        speechSynthesisObj.voice = voices.find((voice, index) => {
+            return voice.lang === 'vi-VN';
+        });
+        speechSynthesisObj.rate = 1;
+        speechSynthesisObj.pitch = 1;
+    }
+    speechSynthesis.speak(speechSynthesisObj);
 }
 
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
+translateBtn.addEventListener('click', translateQuote);
+aloudBtn.addEventListener('click', aloudQuote);
+
 
 // On Load
 getQuotes();
